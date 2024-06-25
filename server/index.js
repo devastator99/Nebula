fd37dd89c5fa8dd0ba177aec289498674c61030d
devastator99 <nsaquib22@gmail.com>
2024-06-25 22:08:45 +0530

    deployment#1

--- server/index.js
+++ server/index.js
@@ -38,12 +38,10 @@ app.get("/ping", (_req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
-);
+
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   },
 });
